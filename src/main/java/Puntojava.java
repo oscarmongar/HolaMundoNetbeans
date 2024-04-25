e7a1422f173e650ec14926861d27c47a3c26f090
Oscar Montaña <oscar.mongar@educa.jcyl.es>
2024-04-25 17:09:53 +0200

    Primer comit del Punto

--- src/main/java/Puntojava.java
+++ src/main/java/Puntojava.java
@@ -8,5 +8,10 @@
  * @author Admin
  */
 public class Puntojava {
-    
+     public static void main(String[] args) {
+        System.out.println("Hello World! ");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        
+    }
 }
