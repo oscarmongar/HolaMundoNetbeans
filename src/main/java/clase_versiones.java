7a90e73d393ecceb8a1da96e7ce5677e996d07a2
Oscar Montaña <oscar.mongar@educa.jcyl.es>
2024-04-25 17:05:25 +0200

    Primer comit del proyecto

--- src/main/java/clase_versiones.java
+++ src/main/java/clase_versiones.java
@@ -2,6 +2,7 @@
 public class clase_versiones {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+         System.out.println("Hello World!");
         
     }
 }
